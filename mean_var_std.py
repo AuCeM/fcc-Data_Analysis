a28a0a2493b297083ad5a5bc5f96b28e8823daf5
Augusto Cesar Martinez <168700417+AuCeM@users.noreply.github.com>
2025-11-18 17:07:34 -0600

    Update mean_var_std.py
    Deleted unnecessary comments

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,33 +2,7 @@ import numpy as np
 
 
 def calculate(arr):
-    # try:
-    #     x = np.array(arr)
-    #     x = x.reshape((3, 3))
-    # except ValueError:
-    #     raise ValueError("List must contain nine numbers.")
-    #
-    # calculations = {
-    #                 'mean': [np.mean(x, axis=0).tolist(),
-    #                          np.mean(x, axis=1).tolist(),
-    #                          float(x.flatten().mean())],
-    #                 'variance': [np.var(x, axis=0).tolist(),
-    #                              np.var(x, axis=1).tolist(),
-    #                              float(x.flatten().var())],
-    #                 'standard deviation': [np.std(x, axis=0).tolist(),
-    #                                        np.std(x, axis=1).tolist(),
-    #                                        float(x.flatten().std())],
-    #                 'max': [np.max(x, axis=0).tolist(),
-    #                         np.max(x, axis=1).tolist(),
-    #                         int(x.flatten().max())],
-    #                 'min': [np.min(x, axis=0).tolist(),
-    #                         np.min(x, axis=1).tolist(),
-    #                         int(x.flatten().min())],
-    #                 'sum': [np.sum(x, axis=0).tolist(),
-    #                         np.sum(x, axis=1).tolist(),
-    #                         int(x.flatten().sum())]
-    #                 }
-
+    
     def function(matrix, func, a=0):
         if func == 'mean':
             return np.mean(matrix, axis=a)
@@ -69,3 +43,4 @@ def calculate(arr):
                     calculations[k].append(int(function(x.flatten(), k)))
 
     return calculations
+
